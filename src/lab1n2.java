b0b1d8a4016e177fb3577048586a14e32e97eeaa
poriesto <poriesto@hotmail.com>
2014-02-14 17:57:52 +0400

    stream cout

--- src/lab1n2.java
+++ src/lab1n2.java
@@ -5,16 +5,14 @@ public class lab1n2 {
     public static void main(String[] argc){
         int year = 0;
         float init_money = Float.parseFloat(argc[0]), percent_per_mo = Float.parseFloat(argc[1]), doxod = (init_money*percent_per_mo)*12, target = init_money*2;
-        System.out.print("Initial data:\n");
-        System.out.print("Initial money = "); System.out.print(init_money); System.out.print("\n");
-        System.out.print("Doxod per mo = "); System.out.print(init_money*percent_per_mo); System.out.print("\n");
-        System.out.print("Doxod per year = "); System.out.print(doxod); System.out.print("\n");
-        System.out.print("Target money = "); System.out.print(target); System.out.print("\n");
+        System.out.println("Initial data:\n" + "Initial money = " + init_money + "\nDoxod per mo = " + init_money*percent_per_mo +
+                "\nDoxod per year = " + doxod + "\nTarget money = " + target + "\n");
 
         while(init_money < target){
             init_money += doxod;
             year++;
         }
+        
         System.out.println("\nResults:" + "\n" + "Final result money = " + init_money + "\n" + "Years = " + year);
     }
 }
