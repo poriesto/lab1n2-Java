f34a1f4d4faeab624643051aa37ff197dcb79bc4
poriesto <poriesto@hotmail.com>
2014-02-14 20:33:22 +0400

    some changes

--- src/lab1n2.java
+++ src/lab1n2.java
@@ -1,6 +1,12 @@
 /**
  * Created by poriesto on 2/14/14.
  */
+/**
+ * Создать приложение с 2 параметрами, которое вычислит количество лет ,за которые сумма денег,положенная в банк, должна удвоится.
+ * В качестве параметров задаются любые неотрицательные числа.
+ *1-ое число - количество денег, 2-ое число-годовой процент.
+ *Учесть, что проценты начисляются раз в месяц и капитализируются.Сосчитать и вывести на дисплей результат подсчета.
+ * */
 public class lab1n2 {
     public static void main(String[] argc){
         int year = 0;
@@ -11,8 +17,11 @@ public class lab1n2 {
         while(init_money < target){
             init_money += doxod;
             year++;
+            if(year > 1){
+                doxod = (init_money*percent_per_mo)*12;
+            }
         }
-        
+
         System.out.println("\nResults:" + "\n" + "Final result money = " + init_money + "\n" + "Years = " + year);
     }
 }
