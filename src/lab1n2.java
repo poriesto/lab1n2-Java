42446951d4aeae5ee8f81e4ad9fd38e93ae281b5
poriesto <poriesto@hotmail.com>
2014-02-15 15:22:05 +0400

    Cleanup

--- src/lab1n2.java
+++ src/lab1n2.java
@@ -10,28 +10,19 @@
  public class lab1n2 {
     public static void main(String[] argv){
         double month = 0, summ_month = 12, year, d_per_m;
-        double init_money = Double.parseDouble(argv[0]), percent_per_year = Double.parseDouble(argv[1])/100, doxod = init_money*percent_per_year, target = 20000;
+        double init_money = Double.parseDouble(argv[0]), percent_per_year = Double.parseDouble(argv[1])/100;
+        double doxod = init_money*percent_per_year, target = init_money*2;
+
         System.out.println("Initial data:\n" + "Initial money = " + init_money + "\nPercent = " + percent_per_year + "%\nDoxod per year = " + doxod + "\nTarget money = " + target);
-/*
-        while(init_money < target){
-            init_money += doxod;
-            System.out.print(init_money);
-            System.out.print("\n");
-            year++;
-            if(year > 0){
-                doxod = init_money*percent_per_year;
-                System.out.print(doxod);
-                System.out.print("\n");
-            }
-        }*/
+
         while(init_money < target){
             d_per_m = init_money*(percent_per_year);
             init_money = init_money + (d_per_m/12);
             month++;
         }
         month = month+1;
-        // year = log(1+percent)X/sum
         year = month/summ_month;
+
         System.out.println("\nResults:" + "\n" + "Final result money = " + init_money + "\n" + "Years = " + year);
     }
 }
