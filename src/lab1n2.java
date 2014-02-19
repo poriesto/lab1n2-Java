23a5584673e3ad1180ac63b1a6c07601c4912b98
poriesto <poriesto@hotmail.com>
2014-02-20 01:29:06 +0400

    new class

--- src/lab1n2.java
+++ src/lab1n2.java
@@ -7,21 +7,50 @@
  *1-ое число - количество денег, 2-ое число-годовой процент.
  *Учесть, что проценты начисляются раз в месяц и капитализируются.Сосчитать и вывести на дисплей результат подсчета.
  * */
- public class lab1n2 {
-    public static void main(String[] argv){
-        double month = 0, summ_month = 12, d_per_m;
-        double init_money = Double.parseDouble(argv[0]), percent_per_year = (Double.parseDouble(argv[1])/100);
-        double doxod = init_money*percent_per_year, target = init_money*2;
+import java.util.*;
+ class Economy{
+    private Double month = 0.0;
+    Integer year = 12;
+    private Double percent_per_year, doxod_per_month, target;
+    private Vector<Double> dpm = new Vector<Double>();
+    public Double init_money;
 
-        System.out.println("Initial data:\n" + "Initial money = " + init_money + "\nPercent = " + percent_per_year + "%\nDoxod per year = " + doxod + "\nTarget money = " + target);
-
+    public void init(String[] str){
+        init_money = Double.parseDouble(str[0]);
+        percent_per_year = Double.parseDouble(str[1])/100;
+        target = init_money*2;
+    }
+    public void print_initial_date(){
+        System.out.println("Initial money: " + init_money + "\nTarget money: " + target + "\nPercent per year: " + percent_per_year*100);
+    }
+    public void print_results(){
+        System.out.println("Result money = " + init_money + "\nYears: " +(month / year) + "\nDoxod per month:");
+        DPM();
+    }
+    private void DPM(){
+        int i = 0;
+        while(i != dpm.size()){
+            System.out.println("\tMonth " + i + " Doxod = " + dpm.elementAt(i));
+            i++;
+        }
+    }
+    public void calculate(){
         while(init_money < target){
-            d_per_m = init_money*(percent_per_year);
-            init_money = init_money + (d_per_m/12);
+            doxod_per_month = (init_money*(percent_per_year))/year;
+            dpm.add(doxod_per_month);
+            init_money = init_money + doxod_per_month;
             month++;
         }
-        month = month+1;
+    }
+}
 
-        System.out.println("\nResults:" + "\n" + "Final result money = " + init_money + "\n" + "Years = " + month/summ_month);
+public class lab1n2 {
+    public static void main(String[] argv){
+
+        Economy ec = new Economy();
+        ec.init(argv);
+        ec.print_initial_date();
+        ec.calculate();
+        ec.print_results();
     }
 }
